d5b7bff013758ead47d8a21817ae943c51f0ea0e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-06-20 17:33:03 +0530

    header

--- build-my-resume-app/src/Components/Header.jsx
+++ build-my-resume-app/src/Components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
-        A <span>Resume</span>
-        <p className={styles.heading}>You can build your resume here <span>It's free</span></p>
+        A <span style={{color:"red"}}>Resume</span>
+        <p className={styles.heading}>You can build your resume here <span style={{color:"red"}}>It's free</span></p>
       </div>
     </div>
   )
